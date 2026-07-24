b7aae1f266c6463129419cec2f4c4344959fbffd
Williams Sandjaya <133097737+nocturnalnerds@users.noreply.github.com>
2026-07-24 13:32:38 +0700

    feat: chat & voice agent sycnh Gcal

--- src/modules/lifePlan/lifeplan.module.ts
+++ src/modules/lifePlan/lifeplan.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../../common/auth.module';
+import { GoogleCalendarModule } from '../google-calendar/google-calendar.module';
 import { LifePlanController } from './lifeplan.controller';
 import { LifePlanService } from './lifeplan.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, GoogleCalendarModule],
   controllers: [LifePlanController],
   providers: [LifePlanService],
   exports: [LifePlanService],
